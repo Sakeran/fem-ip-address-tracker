818eaf7e0e9812bb4fc9aed41c633f73ce29e929
Alexander <Sakeran@gmail.com>
2021-11-10 15:26:55 -0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <div>
+      <h1 class="text-custom-violet fs-900 uppercase">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
