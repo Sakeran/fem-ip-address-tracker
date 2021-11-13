ad8c6508d3625cf58c6b697f7f8da733a50e0e0e
Alexander <Sakeran@gmail.com>
2021-11-12 21:00:39 -0500

    Add error message modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import {
   Component,
-  createEffect,
   createResource,
   createSignal,
+  Show,
 } from "solid-js";
+import { Portal } from "solid-js/web";
+
 import { IpDetails } from "./components/IpDetails";
 import { SearchBar } from "./components/SearchBar";
 import { StreetMap } from "./components/StreetMap";
@@ -20,8 +22,32 @@ const App: Component = () => {
     setQuery(() => newSearch);
   };
 
+  const isError = () => {
+    const data = ipData();
+    if (!data) return false;
+    return ("error" in data);
+  }
+  
+  const errorMsg = () => {
+    const data = ipData();
+    if (data && "error" in data) {
+      return data.error;
+    }
+    return "";
+  }
+
   return (
     <div class={styles.Layout}>
+      <Portal mount={document.getElementById("app-modal") || undefined}>
+        <Show when={isError()}>
+          <div className="app-modal cover">
+            <div class="bg-white pad-400 flex-col text-center center cover-primary">
+              <h2 class="fs-600">Error</h2>
+              <p class="fs-400">{errorMsg()}</p>
+            </div>
+          </div>
+        </Show>
+      </Portal>
       <div class={`${styles.LayoutUI} flow`}>
         <h1 class="text-white fs-600 fw-500 text-center">IP Address Tracker</h1>
 
@@ -37,7 +63,7 @@ const App: Component = () => {
       </div>
 
       <div class={`${styles.MapContainer}`}>
-        <StreetMap details={ipData()}/>
+        <StreetMap details={ipData()} />
       </div>
     </div>
   );
