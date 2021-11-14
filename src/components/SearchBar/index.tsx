92e7ef2e29e569686bad5c8f450473b28311dbc5
Alexander <Sakeran@gmail.com>
2021-11-13 23:22:03 -0500

    Add hint to search bar

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,4 +1,4 @@
-import { Component } from "solid-js";
+import { Component, createSignal, Show } from "solid-js";
 import isIp from "validator/lib/isIp";
 import isFQDN from "validator/lib/isFQDN";
 
@@ -11,7 +11,10 @@ import styles from "./SearchBar.module.scss";
 export const SearchBar: Component<{
   onNewSearch: (newInput: IpLookupTarget) => void;
 }> = (props) => {
+  const [showHint, setHint] = createSignal(false);
+
   let ipSearch: HTMLInputElement | undefined;
+  let hintRef: HTMLParagraphElement | undefined;
 
   const submit = (e: Event) => {
     e.preventDefault();
@@ -34,26 +37,39 @@ export const SearchBar: Component<{
     }
 
     // Otherwise, display a hint
-    // TODO - Display Hint
-    console.log("Invalid Query");
+    setHint(true);
+    if (ipSearch) ipSearch.value = "";
+    hintRef && hintRef.focus();
   };
 
   return (
-    <form role="search" class={styles.SearchBar} onSubmit={submit}>
-      <label class="sr-only" htmlFor="ip-address-search">
-        IP Address to search
-      </label>
-      <input
-        ref={ipSearch}
-        type="search"
-        name="ip-address-search"
-        id="ip-address-search"
-        placeholder="Search for any IP address or domain"
-      />
-      <button onClick={submit}>
-        <span class="sr-only">Search IP</span>
-        <img src={IconArrow} alt="" />
-      </button>
+    <form role="search" onSubmit={submit} class="pos-rel">
+      <Show when={showHint()}>
+        <p
+          tabIndex="0"
+          ref={hintRef}
+          class={`${styles.Hint} pad-200 text-white bg-gray-200`}
+        >
+          Please enter a valid IP address or domain.
+        </p>
+      </Show>
+      <div class={styles.SearchBar}>
+        <label class="sr-only" htmlFor="ip-address-search">
+          IP Address to search
+        </label>
+        <input
+          ref={ipSearch}
+          type="search"
+          name="ip-address-search"
+          id="ip-address-search"
+          placeholder="Search for any IP address or domain"
+          onKeyUp={() => showHint() && setHint(false)}
+        />
+        <button onClick={submit}>
+          <span class="sr-only">Search IP</span>
+          <img src={IconArrow} alt="" />
+        </button>
+      </div>
     </form>
   );
 };
